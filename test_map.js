0eb7cf37a00f3e701afed2311f32fefeafcc7923
Mickaël Thomas <mickael9@gmail.com>
2015-11-05 02:17:56 +0100

    Use require() for JSON loading, fix indentation and test_map.js

--- test_map.js
+++ test_map.js
@@ -1,16 +1,16 @@
 var master = require('./twitch_master'),
+	pub = require('./lib/comm').sender(),
+	map = require('./map.json'),
 	TIMEOUT = 500,
 	length = 0;
 
-master.map_load();
-
 function iter()
 {
-	var next = Object.keys(master.map)[length];
+	var next = Object.keys(map)[length];
   
 	if (next != null) {
-		console.log('Sending: ' + next + ' -> ' + master.map[next]);
-		pub.send(['qemu-manager', master.map[next] + '\n']);
+		console.log('Sending: ' + next + ' -> ' + map[next]);
+		pub.send(['qemu-manager', map[next] + '\n']);
 
 		length++;
 		setTimeout(iter, TIMEOUT);
--- test_rules.js
+++ test_rules.js
@@ -1,51 +1,30 @@
 // This program makes sures the legacy map of commands give the
 // same result in the new rule system
-var fs  = require('fs');
-var rules = require('./lib/rules');
-var util = require('util');
+var	fs = require('fs'),
+	rules = require('./lib/rules'),
+	util = require('util'),
+	map = require('./map.json');
 
-var TIMEOUT = 500;
+var keys = Object.keys(map);
+var errors = [];
 
-// Load json command mapping
-var map = {}
+keys.forEach(function(x) {
+	var resolved = rules.resolve(x);
+	if (resolved !== map[x]) {
+		errors.push([x, resolved, map[x]]);
+	}
+});
 
-function map_load() {
-  fs.exists('map.json', function() {
-    try {
-      var map_new = JSON.parse(fs.readFileSync('map.json', 'utf8'));
-      map = map_new;
-      console.log('(Re)loaded map.json');
-    } catch (ex) {
-      console.log('Could not load map.json');
-      console.log(ex);
-    }
-  });
+if (errors.length != 0) {
+	console.log("Some keys did not match:");
+	errors.forEach(function(x) {
+		console.log("key: " + x[0] + ", got: " + x[1] + ", expected: " + x[2]);
+	});
+} else {
+	console.log("All keys match!");
 }
 
-map_load();
-
-setTimeout(function() {
-  var keys = Object.keys(map);
-  var errors = [];
-
-  keys.forEach(function(x) {
-    var resolved = rules.resolve(x);
-    if (resolved !== map[x]) {
-      errors.push([x, resolved, map[x]]);
-    }
-  });
-
-  if (errors.length != 0) {
-    console.log("Some keys did not match:");
-    errors.forEach(function(x) {
-      console.log("key: " + x[0] + ", got: " + x[1] + ", expected: " + x[2]);
-    });
-  } else {
-    console.log("All keys match!");
-  }
-}, TIMEOUT);
-
 process.stdin.on('data', function(d) {
-  data = d.toString().trim();
-  console.log(data + ": " + rules.resolve(data));
+	data = d.toString().trim();
+	console.log(data + ": " + rules.resolve(data));
 });
